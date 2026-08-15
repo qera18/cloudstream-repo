0beeddc5f7ba74276ae54a24195ff3b7012b41ad
qera18 <65716957+qera18@users.noreply.github.com>
2026-08-16 00:41:11 +0300

    chore: add Dizi gradle module

---
+++ dizi/build.gradle.kts
@@ -0,0 +1,9 @@
+version = 1
+
+cloudstream {
+    description = "https://dizi73.life/"
+    authors = listOf("AutoCloud")
+    status = 1
+    tvTypes = listOf("TvSeries", "Movie")
+    language = "tr"
+}
