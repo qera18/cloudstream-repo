2a89069272d85c9bd833a61850e39ffbbd2eae8c
qera18 <65716957+qera18@users.noreply.github.com>
2026-08-16 00:12:57 +0300

    chore: add DiziTest gradle module

---
+++ dizitest/build.gradle.kts
@@ -0,0 +1,9 @@
+version = 1
+
+cloudstream {
+    description = "https://dizipal5.com"
+    authors = listOf("AutoCloud")
+    status = 1
+    tvTypes = listOf("movie", "series")
+    language = "tr"
+}
