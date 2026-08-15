fb38316d23d32e8bc9ccb6c2adb094c074fc6118
qera18 <65716957+qera18@users.noreply.github.com>
2026-08-15 21:55:49 +0300

    chore: add Dizi filim gradle module

---
+++ dizifilim/build.gradle.kts
@@ -0,0 +1,9 @@
+version = 1
+
+cloudstream {
+    description = "https://www.hdfilmcehennemi.nl/"
+    authors = listOf("AutoCloud")
+    status = 1
+    tvTypes = listOf("Movie", "TvSeries")
+    language = "tr"
+}
