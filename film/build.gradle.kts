db3aeca20567bae01244dcfa5ee3f6f83a960cfc
qera18 <65716957+qera18@users.noreply.github.com>
2026-08-16 01:04:16 +0300

    chore: add Film gradle module

---
+++ film/build.gradle.kts
@@ -0,0 +1,9 @@
+version = 1
+
+cloudstream {
+    description = "https://filmzal.me/"
+    authors = listOf("AutoCloud")
+    status = 1
+    tvTypes = listOf("movie", "tvseries")
+    language = "tr"
+}
