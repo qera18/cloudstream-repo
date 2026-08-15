41575ff72d4f5b2f5a5601151020ce000f62f818
qera18 <65716957+qera18@users.noreply.github.com>
2026-08-15 22:21:17 +0300

    chore: add Asya gradle module

---
+++ asya/build.gradle.kts
@@ -0,0 +1,9 @@
+version = 1
+
+cloudstream {
+    description = "https://asyaminik.com/"
+    authors = listOf("AutoCloud")
+    status = 1
+    tvTypes = listOf("TvSeries", "Movie")
+    language = "tr"
+}
