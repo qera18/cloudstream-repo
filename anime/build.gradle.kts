90183929058feae4586dc00b6dcabb699e28a637
qera18 <65716957+qera18@users.noreply.github.com>
2026-08-15 21:59:55 +0300

    chore: add Anime gradle module

---
+++ anime/build.gradle.kts
@@ -0,0 +1,9 @@
+version = 1
+
+cloudstream {
+    description = "https://animecix.tv/"
+    authors = listOf("AutoCloud")
+    status = 1
+    tvTypes = listOf("TvSeries", "Movie")
+    language = "tr"
+}
