9a83494c7ae999cf747efee591a3eaee89c02bee
qera18 <65716957+qera18@users.noreply.github.com>
2026-08-17 03:44:53 +0300

    Delete film directory

--- film/build.gradle.kts
+++
@@ -1,9 +0,0 @@
-version = 1
-
-cloudstream {
-    description = "https://filmzal.me/"
-    authors = listOf("AutoCloud")
-    status = 1
-    tvTypes = listOf("movie", "tvseries")
-    language = "tr"
-}
