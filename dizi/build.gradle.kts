ebd16cf4ecebfb1f89a1abd8e4213020da595ba3
qera18 <65716957+qera18@users.noreply.github.com>
2026-08-17 03:25:07 +0300

    Delete dizi directory

--- dizi/build.gradle.kts
+++
@@ -1,9 +0,0 @@
-version = 1
-
-cloudstream {
-    description = "https://www.dizimom.surf/"
-    authors = listOf("AutoCloud")
-    status = 1
-    tvTypes = listOf("Movie", "TvSeries")
-    language = "tr"
-}
