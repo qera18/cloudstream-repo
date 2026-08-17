bbc2807b2774cdcbc5fa2e9ae5e863efb754999b
qera18 <65716957+qera18@users.noreply.github.com>
2026-08-17 03:45:10 +0300

    Delete dizitest directory

--- dizitest/build.gradle.kts
+++
@@ -1,9 +0,0 @@
-version = 1
-
-cloudstream {
-    description = "https://dizipal5.com"
-    authors = listOf("AutoCloud")
-    status = 1
-    tvTypes = listOf("movie", "series")
-    language = "tr"
-}
