1e92a4005c743cd2afa6b0aeb29c71f045e49470
qera18 <65716957+qera18@users.noreply.github.com>
2026-08-17 03:44:25 +0300

    Delete dizifilim directory

--- dizifilim/build.gradle.kts
+++
@@ -1,9 +0,0 @@
-version = 1
-
-cloudstream {
-    description = "https://www.hdfilmcehennemi.nl/"
-    authors = listOf("AutoCloud")
-    status = 1
-    tvTypes = listOf("Movie", "TvSeries")
-    language = "tr"
-}
