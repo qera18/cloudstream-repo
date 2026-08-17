529c370e0c94bcc48dd615d7411c83f4e0ce7bbd
qera18 <65716957+qera18@users.noreply.github.com>
2026-08-17 03:51:55 +0300

    Delete dizipal5 directory

--- dizipal5/build.gradle.kts
+++
@@ -1,9 +0,0 @@
-version = 1
-
-cloudstream {
-    description = "https://dizipal.com"
-    authors = listOf("AutoCloud")
-    status = 1
-    tvTypes = listOf("Movie", "TvSeries")
-    language = "tr"
-}
