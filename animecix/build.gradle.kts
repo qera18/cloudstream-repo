abeba18488424f3a84c080b7604562c705d6beb5
qera18 <65716957+qera18@users.noreply.github.com>
2026-08-17 11:13:37 +0300

    Update animecix/build.gradle.kts

---
+++ animecix/build.gradle.kts
@@ -0,0 +1,15 @@
+cloudstream {
+    language = "en"
+    authors = listOf("qera18")
+    status = 1
+    tvTypes = listOf("Anime")
+    iconUrl = "https://www.google.com/s2/favicons?domain=animecix.tv&sz=%size%"
+}
+
+android {
+    namespace = "com.qera18.animecix"
+}
+
+dependencies {
+    implementation("org.jsoup:jsoup:1.17.2")
+}
