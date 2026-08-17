8295a8b72cedb3991429c65375c3949f4a36eda1
qera18 <65716957+qera18@users.noreply.github.com>
2026-08-17 03:52:18 +0300

    Delete asya directory

--- asya/build.gradle.kts
+++
@@ -1,9 +0,0 @@
-version = 1
-
-cloudstream {
-    description = "https://asyaminik.com/"
-    authors = listOf("AutoCloud")
-    status = 1
-    tvTypes = listOf("TvSeries", "Movie")
-    language = "tr"
-}
