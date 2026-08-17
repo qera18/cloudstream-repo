6e2c17666f906c7a67eb78308cb3dd504ba4ff8e
qera18 <65716957+qera18@users.noreply.github.com>
2026-08-17 09:28:24 +0300

    feat(Animecix): sync build.gradle.kts

--- Animecix/build.gradle.kts
+++ Animecix/build.gradle.kts
@@ -1,21 +1,5 @@
-// build.gradle.kts
-plugins {
-    id("com.android.library")
-    id("org.jetbrains.kotlin.android")
-}
-
-android {
-    compileSdk = 32
-
-    defaultConfig {
-        minSdk = 21
-        targetSdk = 32
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
-    }
-    kotlinOptions {
-        jvmTarget = "11"
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
+    compilerOptions {
+        jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_11)
     }
 }
