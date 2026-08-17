61cc16539dcbcc8f3159524130eea713291beb70
qera18 <65716957+qera18@users.noreply.github.com>
2026-08-17 11:05:01 +0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,6 +8,10 @@ buildscript {
         mavenCentral()
         maven { url = uri("https://jitpack.io") }
     }
+    dependencies {
+        classpath("com.android.tools.build:gradle:8.5.2")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.24")
+    }
 }
 
 allprojects {
