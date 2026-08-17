529a22d02cbf09a09a3ec3be635b2c36a0f86630
qera18 <65716957+qera18@users.noreply.github.com>
2026-08-17 18:25:13 +0300

    chore: update root build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,48 +1,5 @@
-import com.android.build.gradle.BaseExtension
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-import org.jetbrains.kotlin.gradle.dsl.JvmTarget
-
-buildscript {
-    repositories {
-        google()
-        mavenCentral()
-        maven { url = uri("https://jitpack.io") }
-    }
-    dependencies {
-        classpath("com.android.tools.build:gradle:8.5.2")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.24")
-        classpath("com.github.recloudstream:gradle:-SNAPSHOT")
-    }
-}
-
-allprojects {
-    repositories {
-        google()
-        mavenCentral()
-        maven { url = uri("https://jitpack.io") }
-    }
-}
-
-subprojects {
-    apply(plugin = "com.android.library")
-    apply(plugin = "kotlin-android")
-    apply(plugin = "com.lagradost.cloudstream3.gradle")
-
-    extensions.configure<BaseExtension> {
-        compileSdkVersion(34)
-        defaultConfig {
-            minSdk = 21
-            targetSdk = 34
-        }
-        compileOptions {
-            sourceCompatibility = JavaVersion.VERSION_11
-            targetCompatibility = JavaVersion.VERSION_11
-        }
-    }
-
-    tasks.withType<KotlinCompile>().configureEach {
-        compilerOptions {
-            jvmTarget.set(JvmTarget.JVM_11)
-        }
-    }
+// Top-level build file
+plugins {
+    id("com.android.application") version "8.5.0" apply false
+    id("kotlin-android") version "1.9.22" apply false
 }
