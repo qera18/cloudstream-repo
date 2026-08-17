dc65a683284092943a7e21c96325a02eb1c306f8
qera18 <65716957+qera18@users.noreply.github.com>
2026-08-17 13:51:59 +0300

    feat: add AnimecixProvider build.gradle.kts

---
+++ AnimecixProvider/build.gradle.kts
@@ -0,0 +1,11 @@
+// Use an integer for version numbers
+version = 1
+
+cloudstream {
+    description = "Animecix.tv için temel arama, ana sayfa ve izleme linki sağlayan CloudStream 3 eklentisi."
+    authors = listOf("qera18")
+    status = 1
+    tvTypes = listOf("Movie", "TvSeries")
+    iconUrl = "https://www.google.com/s2/favicons?domain=animecix.tv&sz=%size%"
+    isCrossPlatform = true
+}
