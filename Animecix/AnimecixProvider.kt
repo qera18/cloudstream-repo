f8821c5c6b2280bc2d04035f502925bb6c375856
qera18 <65716957+qera18@users.noreply.github.com>
2026-08-19 20:53:06 +0300

    Update AnimecixProvider.kt

--- Animecix/AnimecixProvider.kt
+++ Animecix/AnimecixProvider.kt
@@ -1,72 +1,90 @@
-// AnimecixProvider.kt
-import cloudstream.android.providers.Provider
-import cloudstream.android.providers.TvType
-import cloudstream.android.utils.LanguageCode
-import cloudstream.android.utils.LinkType
-import cloudstream.android.utils.SubtitleType
-import cloudstream.android.utils.UrlId
-import com.lagradost.cloudstream3.utils.AppUtils
-import com.lagradost.cloudstream3.utils.ExtractorLink
-import com.lagradost.cloudstream3.utils.Qualities
-import com.lagradost.cloudstream3.utils.SubtitleData
-import java.util.concurrent.TimeUnit
+package com.qera18.animecix
 
-class AnimecixProvider : Provider() {
-    override val mainUrl = "https://animecix.tv"
-    override val name = "Animecix"
-    override val lang = LanguageCode.ENGLISH
-    override val supportsEpisodes = true
-    override val supportedTypes = setOf(TvType.Anime, TvType.OVA)
+import com.lagradost.cloudstream3.*
+import com.lagradost.cloudstream3.utils.*
+import org.jsoup.nodes.Element
+
+class AnimecixProvider : MainAPI() {
+    override var mainUrl = "https://animecix.tv"
+    override var name = "Animecix"
+    override var lang = "tr"
     override val hasMainPage = true
+    override val supportedTypes = setOf(TvType.Anime, TvType.OVA)
+
+    override suspend fun getMainPage(
+        page: Int,
+        request: MainPageRequest
+    ): HomePageResponse? {
+        val document = app.get(mainUrl).document
+        val items = document.select("div.item")
+        
+        val homeItems = items.mapNotNull { item ->
+            val title = item.selectFirst("h2.title")?.text() ?: return@mapNotNull null
+            val href = item.selectFirst("a")?.attr("href") ?: return@mapNotNull null
+            val posterUrl = item.selectFirst("img")?.attr("src")
+            
+            newAnimeSearchResponse(title, fixUrl(href), TvType.Anime) {
+                this.posterUrl = fixUrlNull(posterUrl)
+            }
+        }
+        
+        return newHomePageResponse(
+            listOf(HomePageList("Ana Sayfa", homeItems)),
+            hasNext = false
+        )
+    }
 
     override suspend fun search(query: String): List<SearchResponse> {
         val url = "$mainUrl/search?q=$query"
-        val soup = app.get(url).document
-        val items = soup.select("div.item")
-        return items.map { item ->
-            val title = item.selectFirst("h2.title")!!.text()
-            val id = item.selectFirst("a")!!.href()
-            SearchResponse(
-                title,
-                id,
-                TvType.Anime,
-                UrlId(id)
-            )
+        val document = app.get(url).document
+        val items = document.select("div.item")
+        
+        return items.mapNotNull { item ->
+            val title = item.selectFirst("h2.title")?.text() ?: return@mapNotNull null
+            val href = item.selectFirst("a")?.attr("href") ?: return@mapNotNull null
+            val posterUrl = item.selectFirst("img")?.attr("src")
+            
+            newAnimeSearchResponse(title, fixUrl(href), TvType.Anime) {
+                this.posterUrl = fixUrlNull(posterUrl)
+            }
         }
     }
 
-    override suspend fun loadHome(): List<HomeSearchResponse> {
-        val url = mainUrl
-        val soup = app.get(url).document
-        val items = soup.select("div.item")
-        return items.map { item ->
-            val title = item.selectFirst("h2.title")!!.text()
-            val id = item.selectFirst("a")!!.href()
-            HomeSearchResponse(
-                title,
-                id,
-                TvType.Anime,
-                UrlId(id)
-            )
+    override suspend fun load(url: String): LoadResponse? {
+        val document = app.get(url).document
+        val title = document.selectFirst("h1.title")?.text() ?: return null
+        val posterUrl = document.selectFirst("div.poster img")?.attr("src")
+        val plot = document.selectFirst("div.description")?.text()
+
+        return newAnimeLoadResponse(title, url, TvType.Anime) {
+            this.posterUrl = fixUrlNull(posterUrl)
+            this.plot = plot
         }
     }
 
-    override suspend fun loadEpisode(episodeId: String): EpisodeData {
-        val url = episodeId
-        val soup = app.get(url).document
-        val title = soup.selectFirst("h1.title")!!.text()
-        val links = soup.select("div.player")
-        val episodeLinks = links.map { link ->
-            ExtractorLink(
-                link.selectFirst("iframe")!!.attr("src"),
-                "Animecix",
-                Qualities.P1080p.value,
-                LinkType.VIDEO
-            )
+    override suspend fun loadLinks(
+        data: String,
+        isCdn: Boolean,
+        handler: PlaylistUtils,
+        callback: (ExtractorLink) -> Unit
+    ): Boolean {
+        val document = app.get(data).document
+        val iframes = document.select("div.player iframe")
+        
+        iframes.forEach { iframe ->
+            val src = iframe.attr("src")
+            if (src.isNotEmpty()) {
+                callback(
+                    ExtractorLink(
+                        name = this.name,
+                        source = this.name,
+                        url = fixUrl(src),
+                        referer = mainUrl,
+                        quality = Qualities.P1080.value
+                    )
+                )
+            }
         }
-        return EpisodeData(
-            title,
-            episodeLinks
-        )
+        return true
     }
 }
