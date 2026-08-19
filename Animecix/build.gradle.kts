c7ef5f7d9517d24711950669371a331b0836b98e
qera18 <65716957+qera18@users.noreply.github.com>
2026-08-19 19:42:39 +0300

    Update build.gradle.kts

--- Animecix/build.gradle.kts
+++ Animecix/build.gradle.kts
@@ -1,16 +1,25 @@
 plugins {
-    id("com.android.application")
-    id("kotlin-android")
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
+    id("com.lagradost.cloudstream3.gradle")
+}
+
+version = 1
+
+cloudstream {
+    description = "Animecix"
+    authors = listOf("qera18")
+    status = 1
+    tvTypes = listOf("Anime")
+    iconUrl = "https://www.google.com/s2/favicons?domain=animecix.tv&sz=%size%"
 }
 
 android {
     namespace = "com.qera18.animecix"
     compileSdk = 34
-
     defaultConfig {
         minSdk = 21
     }
-
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
@@ -22,4 +31,7 @@ android {
 
 dependencies {
     cloudstream("com.lagradost:cloudstream3:pre-release")
+    implementation(kotlin("stdlib"))
+    implementation("com.github.Blatzar:NiceHttp:0.4.11")
+    implementation("org.jsoup:jsoup:1.18.3")
 }
