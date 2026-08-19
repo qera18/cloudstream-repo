5f6beea9c821857be1915e09f3df002e957e467a
qera18 <65716957+qera18@users.noreply.github.com>
2026-08-19 21:04:48 +0300

    chore: update root build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,30 +1,5 @@
-import com.lagradost.cloudstream3.gradle.CloudstreamExtension
-
-buildscript {
-    repositories {
-        google()
-        mavenCentral()
-        maven("https://jitpack.io")
-    }
-    dependencies {
-        classpath("com.android.tools.build:gradle:8.5.0")
-        // Kotlin sürümünü CloudStream'in güncel ABI standartlarına uygun sürüme çekiyoruz
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.1.0")
-        classpath("com.github.recloudstream:gradle:-SNAPSHOT")
-    }
-}
-
-allprojects {
-    repositories {
-        google()
-        mavenCentral()
-        maven("https://jitpack.io")
-    }
-}
-
-fun Project.cloudstream(configuration: CloudstreamExtension.() -> Unit) =
-    extensions.getByName<CloudstreamExtension>("cloudstream").configuration()
-
-tasks.register("clean", Delete::class) {
-    delete(rootProject.layout.buildDirectory)
+// Top-level build file
+plugins {
+    id("com.android.application") version "8.5.0" apply false
+    id("kotlin-android") version "1.9.22" apply false
 }
