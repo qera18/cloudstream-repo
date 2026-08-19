4bf891dbed29eecbbda77b0937216653ca98b8b1
qera18 <65716957+qera18@users.noreply.github.com>
2026-08-19 17:40:03 +0300

    feat: add AnimeCix module build.gradle.kts

---
+++ AnimeCix/build.gradle.kts
@@ -0,0 +1,25 @@
+plugins {
+    id("com.android.application")
+    id("kotlin-android")
+}
+
+android {
+    namespace = "com.qera18.animecix"
+    compileSdk = 34
+
+    defaultConfig {
+        minSdk = 21
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
+dependencies {
+    compileOnly("com.lagradost:cloudstream3:1.0.0")
+}
