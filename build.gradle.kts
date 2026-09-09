ae21b39eb6cc3d61d80b48b7f34a1ba725626ec8
qera18 <65716957+qera18@users.noreply.github.com>
2026-09-09 09:26:05 +0300

    chore: update build.gradle.kts [CloudStream Plugin Builder]

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,3 @@
-import com.android.build.api.dsl.LibraryExtension
-import com.lagradost.cloudstream3.gradle.CloudstreamExtension
-import org.gradle.kotlin.dsl.register
-import org.jetbrains.kotlin.gradle.dsl.JvmTarget
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-import org.jetbrains.kotlin.gradle.tasks.KotlinJvmCompile
-
 buildscript {
     repositories {
         google()
@@ -16,11 +9,10 @@ buildscript {
             }
         }
     }
-
     dependencies {
-        classpath("com.android.tools.build:gradle:9.1.1")
+        classpath("com.android.tools.build:gradle:8.1.2")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.22")
         classpath("com.github.recloudstream.gradle:gradle:gradle-master-32895aedb6-1")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.3.21")
     }
 }
 
@@ -30,79 +22,4 @@ allprojects {
         mavenCentral()
         maven("https://jitpack.io")
     }
-}
-
-subprojects {
-    tasks.withType<KotlinCompile>().configureEach {
-        compilerOptions {
-            freeCompilerArgs.add("-Xannotation-default-target=param-property")
-        }
-    }
-}
-
-fun Project.cloudstream(configuration: CloudstreamExtension.() -> Unit) = extensions.getByName<CloudstreamExtension>("cloudstream").configuration()
-
-fun Project.android(configuration: LibraryExtension.() -> Unit) {
-    extensions.getByName<LibraryExtension>("android").apply {
-        configuration()
-    }
-}
-
-subprojects {
-    apply(plugin = "com.android.library")
-    apply(plugin = "com.lagradost.cloudstream3.gradle")
-
-    cloudstream {
-        // CI'da GITHUB_REPOSITORY ortam değişkeni kullanılır
-        setRepo(System.getenv("GITHUB_REPOSITORY") ?: "https://github.com/qera18/cloudstream-repo")
-        authors = listOf("qera18")
-    }
-
-    android {
-        namespace = "com.qera18.plugin"
-        compileSdk = 36
-
-        defaultConfig {
-            minSdk = 21
-        }
-
-        lint {
-            targetSdk = 36
-        }
-
-        compileOptions {
-            sourceCompatibility = JavaVersion.VERSION_1_8
-            targetCompatibility = JavaVersion.VERSION_1_8
-        }
-
-        tasks.withType<KotlinJvmCompile> {
-            compilerOptions {
-                jvmTarget.set(JvmTarget.JVM_1_8)
-                freeCompilerArgs.addAll(
-                    "-Xno-call-assertions",
-                    "-Xno-param-assertions",
-                    "-Xno-receiver-assertions",
-                    "-Xannotation-default-target=param-property"
-                )
-            }
-        }
-    }
-
-    dependencies {
-        val implementation by configurations
-        val cloudstream by configurations
-
-        // CloudStream sınıfları (stub) — derleme için gerekli
-        cloudstream("com.lagradost:cloudstream3:pre-release")
-
-        implementation(kotlin("stdlib"))
-        implementation("com.github.Blatzar:NiceHttp:0.4.11")
-        implementation("org.jsoup:jsoup:1.18.1")
-        implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.16.0")
-        implementation("com.fasterxml.jackson.core:jackson-databind:2.16.0")
-    }
-}
-
-tasks.register<Delete>("clean") {
-    delete(rootProject.layout.buildDirectory)
 }
