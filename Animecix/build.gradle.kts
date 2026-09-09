377dc2b64c54f912f07916aaabc6e1b88c983706
qera18 <65716957+qera18@users.noreply.github.com>
2026-09-09 04:34:46 +0300

    feat: add Animecix module build.gradle.kts

--- Animecix/build.gradle.kts
+++ Animecix/build.gradle.kts
@@ -1,3 +1,15 @@
+version = 1
+
+cloudstream {
+    description = "Animecix plugin for CloudStream"
+    authors = listOf("qera18")
+    status = 1
+    tvTypes = listOf("Anime", "TvSeries", "Movie", "OVA", "ONA", "Special")
+    language = "tr"
+    requiresResources = false
+    iconUrl = "https://www.google.com/s2/favicons?domain=animecix.tv&sz=%size%"
+}
+
 android {
     namespace = "com.qera18.animecix"
 }
