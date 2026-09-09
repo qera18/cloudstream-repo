900d2ec6d14111ebdbf94caa862e14db0b6b85f2
qera18 <6.5716957e+07+qera18@users.noreply.github.com>
2026-09-09 07:12:40 +0000

    fix: register Belgeselx CloudStream plugin

---
+++ belgeselx/src/main/java/com/cloudstream/belgeselx/BelgeselxPlugin.kt
@@ -0,0 +1,12 @@
+package com.cloudstream.belgeselx
+
+import android.content.Context
+import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
+import com.lagradost.cloudstream3.plugins.Plugin
+
+@CloudstreamPlugin
+class BelgeselxPlugin : Plugin() {
+    override fun load(context: Context) {
+        registerMainAPI(BelgeselxProvider())
+    }
+}
